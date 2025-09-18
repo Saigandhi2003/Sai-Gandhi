c3be5867203c014e5caa75834f5aa74dcc209d8e
Saigandhithota <157969500+Saigandhi2003@users.noreply.github.com>
2025-09-18 11:25:23 +0530

    problem-1.py

--- problem-1.py
+++ problem-1.py
@@ -1,1 +1,28 @@
+class Calculator:
+    def __init__(self, a: float, b: float, operation: str):
+        self.a = a
+        self.b = b
+        self.operation = operation.lower()
 
+    def calculate(self):
+        if self.operation == "addition":
+            return self.a + self.b
+        elif self.operation == "subtraction":
+            return self.a - self.b
+        elif self.operation == "multiplication":
+            return self.a * self.b
+        elif self.operation == "division":
+            if self.b == 0:
+                return "Error: Division by zero is not allowed"
+            return self.a / self.b
+        else:
+            return "Invalid operation type"
+          
+if __name__ == "__main__":
+    a = float(input("Enter first number : "))
+    b = float(input("Enter second number : "))
+    operation = input("Enter operation (addition, subtraction, multiplication, division): ")
+
+    calc = Calculator(a, b, operation)
+    result = calc.calculate()
+    print("Result:", result)
